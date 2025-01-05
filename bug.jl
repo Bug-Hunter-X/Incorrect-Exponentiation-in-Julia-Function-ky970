50bd7ef801218a8809bc4170cc8707809789d955
BugHunter-X <askzenai@gmail.com>
2025-01-05 23:12:04 +0100

    Add bug.jl file

---
+++ bug.jl
@@ -0,0 +1,17 @@
+```julia
+function my_function(x)
+  if x > 10
+    return x^2
+  else
+    return x + 1
+  end
+end
+
+result = my_function(5)
+println(result) # Output: 6
+
+result = my_function(15)
+println(result) # Output: 225
+
+#This is wrong. It should return x*x instead of x^2
+```
